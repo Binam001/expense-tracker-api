24cd14de2258072b82a0620eb625da299f9b7fca
Binam001 <binamshrestha770@gmail.com>
2025-09-26 08:24:27 +0545

    added cron job

--- src/server.js
+++ src/server.js
@@ -3,20 +3,22 @@ import dotenv from "dotenv"
 import ratelimiter from "./middleware/rateLimiter.js";
 import transactionsRoute from "./routes/transactionsRoute.js"
 import { initDB } from "./config/db.js";
+import job from "./config/cron.js";
+
+dotenv.config()
 
 const app = express();
 
+if(process.env.NODE_ENV === "production") job.start();
 //middleware
 app.use(ratelimiter)
 app.use(express.json());
 
-dotenv.config()
-
 const PORT = process.env.PORT || 5001;
 
-// app.get('/', (req, res) => {
-//   res.send("Hello, World")
-// })
+app.get('/api/health', (req, res) => {
+  res.status(200).json({status: "OK"})
+})
 
 app.use("/api/transactions", transactionsRoute)
 
